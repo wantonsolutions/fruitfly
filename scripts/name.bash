9f70207762f6beec0d495774b86f4d4d3a8fc7df
wantonsolutions <stewbertgrant@gmail.com>
2018-12-12 20:35:15 -0800

    renaming occurs at a global level

--- scripts/name.bash
+++ scripts/name.bash
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+#this scripts renames every jpg in a folder suppiled on the command line to a
+#numeric id
 
-i=1
-for f in *.jpg; do
-    mv $f $i.jpg
-    let "i++"
-done
+
+function rename() {
+    cd $1
+    i=1
+    for f in *.jpg; do
+        echo "Moving $1$f -> $1$i.jpg"
+        mv $f $i.jpg
+        let "i++"
+    done
+}
+
+echo "Renaming $1"
+rename $1
